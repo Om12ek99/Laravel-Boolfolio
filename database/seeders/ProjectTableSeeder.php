5469766fff771247a677a8b448cce718006dbfec
Om12ek99 <omar.elkamili@gmail.com>
2024-06-19 23:37:06 +0200

    add: faker to seeder

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+//serve per poter usare slug
+use Illuminate\Support\Str;
 
 class ProjectTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+
+        for ($i = 0; $i < 10; $i++) {
+        $newProject = new Project();
+        $newProject->title = $faker->sentence(3);
+        $newProject->content = $faker->text(500);
+        //mette il trattino tra una parola e l'altra prendendo titolo come riferimento
+        $newProject->slug = Str::slug($newProject->title);
+        $newProject->save();
+        }
     }
 }
