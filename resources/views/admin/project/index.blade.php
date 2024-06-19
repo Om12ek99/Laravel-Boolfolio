b8c6fb597a1ea6d37c086206473cae5fa781160b
Om12ek99 <omar.elkamili@gmail.com>
2024-06-20 00:24:37 +0200

    fix: variable name in index

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -6,7 +6,7 @@
     <a href="{{ route('admin.project.create') }}" class="btn btn-primary mb-3">Crea nuovo progetto</a>
 
     <ul>
-        @foreach ($newProject as $project)
+        @foreach ($projects as $project)
             <li>{{ $project->title }}</li>
         @endforeach
     </ul>
