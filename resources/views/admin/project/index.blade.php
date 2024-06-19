7e870fd8e6d034dbd5bb509bdd68842a8f9cc587
Om12ek99 <omar.elkamili@gmail.com>
2024-06-20 00:17:26 +0200

    add: in index added route to create.blade and a foreach cicle to show created files

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -1,4 +1,13 @@
-@extends('layout.admin')
+@extends('layouts.admin')
+
 @section('content')
-    <h1>ciao</h1>
+    <h1>Progetti</h1>
+
+    <a href="{{ route('admin.project.create') }}" class="btn btn-primary mb-3">Crea nuovo progetto</a>
+
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
 @endsection
