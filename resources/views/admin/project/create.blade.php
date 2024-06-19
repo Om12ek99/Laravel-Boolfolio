7ef80827586c00dbb6656034d81275c69780aef8
Om12ek99 <omar.elkamili@gmail.com>
2024-06-20 00:15:52 +0200

    add: create.blade with form

---
+++ resources/views/admin/project/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Crea un nuovo progetto</h1>
+
+    <form action="{{ route('admin.project.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Titolo</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+        </div>
+        <div class="form-group">
+            <label for="content">Contenuto</label>
+            <textarea class="form-control" id="content" name="content" rows="5">{{ old('content') }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Salva</button>
+    </form>
+@endsection
