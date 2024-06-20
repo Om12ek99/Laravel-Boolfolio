bada3c5344b6a3c9d2cf6f070be0a9ffebdd1dc8
Om12ek99 <omar.elkamili@gmail.com>
2024-06-20 14:59:34 +0200

    fix: newproject capitalized

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -5,10 +5,33 @@
 
     <a href="{{ route('admin.project.create') }}" class="btn btn-primary mb-3">Crea nuovo progetto</a>
 
-    <ul>
-        @foreach ($projects as $curProject)
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Slug</th>
+                <th>Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $curProject)
+                <tr>
+                    <td>{{ $curProject->id }}</td>
+                    <td>{{ $curProject->title }}</td>
+                    <td>{{ $curProject->slug }}</td>
+                    <td>
 
-            <li>{{ $curProject->title }}</li>
-        @endforeach
-    </ul>
+                    <!-- chiedi ai tutor perche mi prende newProject maiuscolo -->
+                        <a href="{{ route('admin.project.show',['NewProject'=>$curProject->slug])}}" class="btn btn-info">Mostra</a>
+                        <form action="{{ route('admin.project.destroy', $curProject->id) }}" method="POST" style="display:inline-block;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Elimina</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
